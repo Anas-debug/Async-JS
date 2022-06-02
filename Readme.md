97a86681c46d829cafd2b640c281f06498239faf
Anas Saoui <62400765+Anas-debug@users.noreply.github.com>
2022-06-02 20:42:48 +0100

    first commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# Topics
+
+## 1-Asyncronous JavaScript.
+## 2-Synchronous vs Asynchronous.
+## 3-Callbacks.
+## 4-Promises.
+## 5-Async/Await.
+## 6-Conclusion.
---
+++ synchronous.js
@@ -0,0 +1,13 @@
+// synchronous
+
+console.log(" I ");
+
+console.log(" eat ");
+
+setTimeout(() => {console.log(" ice cream ");}, 3000);
+
+console.log(" spoon ");
+
+console.log(" with a ");
+
+
