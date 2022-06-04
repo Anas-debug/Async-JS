a5f989d08b830225a594bacda522ff414ef05c0c
Anas Saoui <62400765+Anas-debug@users.noreply.github.com>
2022-06-04 15:02:24 +0100

    async order function

--- ice-cream-store.js
+++ ice-cream-store.js
@@ -7,10 +7,14 @@ let stocks = {
 };
 
 let order = (Fruit_name, call_production) => {
+    setTimeout(() => {
+        console.log(`${stocks.Fruits[Fruit_name]} was selected`);
+    }, 2000);
+
     call_production();
 };
 
 // once we get the order we can start producing ice-cream
 let production = () => {};
 
-order("", production);
+order(0, production);
